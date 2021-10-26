5ba541e3cae0d615c2a9831ff0e936c485885eaf
vysh1234 <vyshnavivinjam@gmail.com>
2021-10-26 17:27:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TPL_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo for task parallel library!");
+
+            //first method
+            var task1 = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("This is the first method to create and start a task using TPL");
+            });
+            task1.Wait();
+            //Second method to create a Task using TPL  
+            var task2 = new Task(() =>
+            {
+                Console.WriteLine("This is the second method to create a task using TPL");
+            });
+            // Next, we have to start the Task  
+            task2.Start();
+            task2.Wait();
+            Console.Write("Program Complete...");
+            Console.ReadKey();
+        }
+    }
+ }
+
